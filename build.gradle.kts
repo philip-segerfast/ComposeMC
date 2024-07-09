6a7a3dbb0642eecb4cf810d52bbe69fb7c623c41
Philip Segerfast <psegerfast@gmail.com>
2024-07-10 00:17:52 +0200

    Mod compiles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,21 @@
+import org.jetbrains.kotlin.com.intellij.openapi.vfs.StandardFileSystems.jar
+
+val modVersion: String by project
+val modGroupId: String by project
+
 plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     alias(libs.plugins.jetbrainsCompose) apply false
 
-    id 'idea'
-    id 'net.neoforged.gradle.userdev' version '7.0.145' apply false
+    id("idea")
+    id("net.neoforged.gradle.userdev") version "7.0.145" apply false
 
     // Adds the Kotlin Gradle plugin
-    id 'org.jetbrains.kotlin.jvm' version "2.0.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
     // OPTIONAL Kotlin Serialization plugin
     //id 'org.jetbrains.kotlin.plugin.serialization' version '2.0.0'
 
-    id 'org.jetbrains.kotlinx.atomicfu' version '0.25.0' apply false
+    id("org.jetbrains.kotlinx.atomicfu") version "0.25.0" apply false
 }
 
 version = modVersion
@@ -20,21 +25,18 @@ allprojects {
     repositories {
         mavenLocal()
         // Add KFF Maven repository
-        maven {
+        maven("https://thedarkcolour.github.io/KotlinForForge/") {
             name = "Kotlin for Forge"
-            url = "https://thedarkcolour.github.io/KotlinForForge/"
-            content { includeGroup "thedarkcolour" }
+            content { includeGroup("thedarkcolour") }
         }
-        maven {
-            url = "https://plugins.gradle.org/m2/"
-        }
+        maven("https://plugins.gradle.org/m2/")
         mavenCentral()
         google()
     }
 }
 
 
-tasks.named("wrapper").configure {
+tasks.named<Wrapper>("wrapper").configure {
     // Define wrapper values here to not have to always do so when updating gradlew.properties.
     // Switching this to Wrapper.DistributionType.ALL will download the full gradle sources that comes with
     // documentation attached on cursor hover of gradle classes and methods. However, this comes with increased
